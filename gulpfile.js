9261b396734e2c0b52c5335bb003d713b42388fa
marktao <marktao1995@qq.com>
2016-05-21 00:04:32 +0800

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var es2015 = require('babel-preset-es2015');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+
+gulp.task('babel',function() {
+    gulp.src('./src/js/*.js')
+        .pipe(babel({
+            presets:[es2015]
+        }))
+        .pipe(gulp.dest('./dest/js'));
+});
+
+gulp.task('sass',function() {
+    gulp.src('./src/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dest/css'));
+});
+
+gulp.task('sass-prefix',['sass'],function() {
+    gulp.src('./dest/css/*.css')
+        .pipe(prefix())
+        .pipe(gulp.dest('./dest/css'));
+});
+
+gulp.task('default',function() {
+    gulp.watch('./src/sass/*.scss',['sass-prefix']);
+    gulp.watch('./src/js/*.js',['babel']);
+})
